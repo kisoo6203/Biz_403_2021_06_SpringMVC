410bd5dc67c9e44bfb6f7a2b9e94759958c004e8
kisoo6203 <kisoo6203@naver.com>
2021-06-29 16:44:41 +0900

    06-28

--- Weather_Ootd/src/main/java/com/callor/woo/controller/HomeController.java
+++ Weather_Ootd/src/main/java/com/callor/woo/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.callor.woo;
+package com.callor.woo.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,29 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * Handles requests for the application home page.
  */
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		return "join";
+	}
+	@RequestMapping(value = "/join/",method=RequestMethod.GET)
+	public String join() {
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "join";
 	}
 	
 }
